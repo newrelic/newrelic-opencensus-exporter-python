e6284c2cdb4bb97388e613a554aec925451ae864
Rey Abolofia <rabolofia@newrelic.com>
2019-05-07 13:55:44 -0700

    Be sure to add all tags as attributes.

--- src/ocnewrelic/stats.py
+++ src/ocnewrelic/stats.py
@@ -91,7 +91,7 @@ class NewRelicStatsExporter(object):
                 _tags.update(zip(view.columns, label_values))
 
                 nr_metric = GaugeMetric(
-                    name=name, value=value, tags=tags, end_time_ms=end_time_ms
+                    name=name, value=value, tags=_tags, end_time_ms=end_time_ms
                 )
                 nr_metrics.append(nr_metric)
 
